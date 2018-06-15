8e5e9e6f66dc9f5a76dfd18f5773261187c5aa9b
Alonso506 <alonsoguzman.217@gmail.com>
2018-06-15 11:47:53 -1000

    Componente agregar producto

---
+++ src/app/agregar-producto/agregar-producto.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import {MaterializeModule} from 'angular2-materialize';
+
+@Component({
+  selector: 'app-agregar-producto',
+  templateUrl: './agregar-producto.component.html',
+  styleUrls: ['./agregar-producto.component.css']
+})
+export class AgregarProductoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+    
+
+
+}
